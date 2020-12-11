24f66eb285f979eba1be45badfd79df03f1e004f
Chad Troyer <thebigticket7575@gmail.com>
2020-12-10 19:32:47 -0500

    html to pdf

--- makePDF.js
+++ makePDF.js
@@ -1,13 +1,16 @@
-import { jsPDF } from "https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.1.1/jspdf.es.min.js";
-
 document.getElementById("PDF").addEventListener("click", function() {
-    let text = "test"
-    console.log(imgData);
-    var doc = new jsPDF()
-
-    doc.setFontSize(40)
-    doc.text(text + "YES" ,35 ,35);
-    doc.addImage (`http://placehold.jp/99ccff/003366/150x150.png`, "PNG", 15, 40, 50, 50)
-    doc.save("this.pdf")
+    var doc = new jsPDF();
+    var elementHTML = $('#contnet').html();
+    var specialElementHandlers = {
+        '#elementH': function (element, renderer) {
+            return true;
+        }
+    };
+    doc.fromHTML(elementHTML, 15, 15, {
+        'width': 170,
+        'elementHandlers': specialElementHandlers
+    });
+    
+    // Save the PDF
+    doc.save('sample-document.pdf');
 })
-
